f98eb32119d0a56f41fe536c7815a43ba5d4d2a2
Gunashekar <Gunashekarreddy057@gmail.com>
2023-11-03 12:33:43 +0530

    Expense-food

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const ExpenseItem = () => {
   return (
     <div>
-      <h2>ExpenseItem!</h2>
+      <h1>ExpenseItem!</h1>
+      <h2>Food Rs 10</h2>
+      <h2>Petrol Rs 100</h2>
+      <h2>Movies Rs 200</h2>
     </div>
-  )
-}
+  );
+};
 
-export default ExpenseItem
+export default ExpenseItem;
