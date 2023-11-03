78673391a1ca9ddd2ea6919ce3ab70d8b343a25d
Gunashekar <Gunashekarreddy057@gmail.com>
2023-11-03 12:27:06 +0530

    Expense-item;

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ExpenseItem from './components/ExpenseItem';
+import './App.css';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h2>lets get started</h2>
+     <ExpenseItem />
+    </div>
+  )
+}
+
+export default App
