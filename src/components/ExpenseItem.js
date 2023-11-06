e784e77e3a98f034cdbb35ed98f2dddf56589b49
Gunashekar <Gunashekarreddy057@gmail.com>
2023-11-06 12:35:08 +0530

    new commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import React from "react";
-
-const ExpenseItem = () => {
+import "./ExpenseItem.css";
+const ExpenseItem = (props) => {
   return (
-    <div>
-      <h1>ExpenseItem!</h1>
-      <h2>Food Rs 10</h2>
-      <h2>Petrol Rs 100</h2>
-      <h2>Movies Rs 200</h2>
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 };
